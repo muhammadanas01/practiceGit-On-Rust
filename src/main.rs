376400bcf0f390b852e8d709b6e478d77ecaacbb
muhammadanas01 <56517089+muhammadanas01@users.noreply.github.com>
2019-12-02 02:31:45 +0500

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-//     let mut  to_print = String::new();
-//    println!("Number 1");
-//    std::io::stdin().read_line(&mut to_print);
+     let mut  to_print = String::new();
+    println!("Number 1");
+    std::io::stdin().read_line(&mut to_print);
 
-//    let mut Number = String::new();
-//    println!("Number 2" );
-//    std::io::stdin().read_line(&mut Number);
+    let mut Number = String::new();
+    println!("Number 2" );
+    std::io::stdin().read_line(&mut Number);
    
-//     println!("The 1st number is: {}",to_print );
-//     println!("The 2nd number is: {}",Number );
+     println!("The 1st number is: {}",to_print );
+     println!("The 2nd number is: {}",Number );
      let mut num=0;
       loop{
           println!("Hello world {}", num );
