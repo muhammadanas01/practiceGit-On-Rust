3d4ebddc1a6f1efe112cb574b4a8646cd44fa09f
Muhammad Anas <anas.aaa01@gmail.com>
2019-12-01 19:21:54 +0500

    added all files

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+//     let mut  to_print = String::new();
+//    println!("Number 1");
+//    std::io::stdin().read_line(&mut to_print);
+
+//    let mut Number = String::new();
+//    println!("Number 2" );
+//    std::io::stdin().read_line(&mut Number);
+   
+//     println!("The 1st number is: {}",to_print );
+//     println!("The 2nd number is: {}",Number );
+     let mut num=0;
+      loop{
+          println!("Hello world {}", num );
+                    num = num + 1;
+         if num==200{
+          break;
+         }
+
+      }
+
+
+    
+}
